e5a42a7e7558a2e5b461f96e7db82b96f747dcd3
vaishnavipardeshi <vaishnavipardeshi22@gmail.com>
2020-02-06 19:26:06 +0530

    ADDED : Basic Shell Script Programs by using Dictionary

---
+++ Dictionary/BirthDays.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -x
+
+# VARIABLE INITIALIZATION
+limit=50
+
+# DECLARE DICTIONARY
+declare -A month
+
+# CALCULATE 50 RANDOM MONTHS
+for (( i=1; i<=$limit; i++ ))
+do
+	random=$(( 1 + RANDOM % 12 ))
+	month[$random]=$(( ${month[$random]} + 1 ))
+done
+
+# DISPLAY DICTIONARY VALUES AND THEIR KEYS
+echo "Birth month count: " ${month[@]}
+echo ${!month[@]}
+
+# DISPLAY INDIVIDUALS HAVING BIRTHDAYS IN THE SAME MONTH
+for (( i=1; i<=12; i++ ))
+do
+	echo $i=${month[$i]}
+done
+
---
+++ Dictionary/RollDie.sh
@@ -0,0 +1,44 @@
+#!/bin/bash -x
+
+# VARIABLE INITIALIZATION
+random=0
+
+# DECLARE DICTIONARY
+declare -A rollDie
+
+# STORE COUNT OF DIE ROLL IN DICTIONARY
+while [[ ${rollDie[$random]} -ne 10 ]]
+do
+	random=$(( 1 + RANDOM % 6 ))
+	rollDie[$random]=$(( ${rollDie[$random]} + 1 ))
+done
+
+echo ${!rollDie[@]}
+echo ${rollDie[@]}
+
+# VARIABLE INITIALIZATION FOR MINIMUM AND MAXIMUM COUNT OF DICE AND DICE NUMBER
+minCount=${rollDie[1]}
+maxCount=${rollDie[1]}
+minDieNum=1
+maxDieNum=1
+
+# CALCULATING DICE NUMBER WHO REACH MAXIMUM AND MINIMUM COUNT
+for (( i=1; i<${#rollDie[@]}; i++))
+do
+	if [[ $minCount -gt ${rollDie[$((i+1))]} ]]
+	then
+		minCount=${rollDie[$((i+1))]}
+		minDieNum=$((i+1))
+	fi
+	if [[ ${rollDie[$((i+1))]} -gt $maxCount ]]
+	then
+		maxCount=${rollDie[$((i+1))]}
+		maxDieNum=$((i+1))
+	fi
+done
+
+# DISPLAY RESULT
+echo minCount=$minCount
+echo minDieNum=$minDieNum
+echo maxCount=$maxCount
+echo maxDieNum=$maxDieNum
