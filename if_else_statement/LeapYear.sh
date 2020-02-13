a21e626935ae3a389cc73941be1844252ac359d4
vaishnavipardeshi <vaishnavipardeshi22@gmail.com>
2020-02-13 12:03:27 +0530

    ADDED : Boundary Condition Check For Year

--- if_else_statement/LeapYear.sh
+++ if_else_statement/LeapYear.sh
@@ -4,19 +4,24 @@
 read -p "Enter Year: " year
 
 # TO CHECK YEAR IS LEAP YEAR OR NOT
-if [ $(( $year % 4 )) -eq 0 ]
+if [ $year -gt 999 -a $year -le 9999 ]
 then
-	if [ $(( $year % 100 )) -eq 0 ]
+	if [ $(( $year % 4 )) -eq 0 ]
 	then
-		if [ $(( $year % 400 )) -eq 0 ]
+		if [ $(( $year % 100 )) -eq 0 ]
 		then
-			echo $year is a Leap Year.
+			if [ $(( $year % 400 )) -eq 0 ]
+			then
+				echo $year is a Leap Year.
+			else
+				echo $year is not a leap Year.
+			fi
 		else
-			echo $year is not a leap Year.
+			echo $year is a Leap Year.
 		fi
 	else
-		echo $year is a Leap Year.
+		echo $year is not a Leap Year.
 	fi
 else
-	echo $year is not a Leap Year.
+	echo "Enter four digit year."
 fi
