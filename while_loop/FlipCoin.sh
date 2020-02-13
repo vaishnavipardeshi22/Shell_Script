6f9f6402de2b6fb66fef18699a4373d1fac6f8d4
vaishnavipardeshi <vaishnavipardeshi22@gmail.com>
2020-02-13 12:26:11 +0530

    REFACTOR : Removed Unwanted variable Modified Variable Name

--- while_loop/FlipCoin.sh
+++ while_loop/FlipCoin.sh
@@ -2,29 +2,28 @@
 
 # INITIALIZE VARIABLE
 head=1
-tail=0
-counter1=0
-counter2=0
+headCounter=0
+tailCounter=0
 
 # CALCULATE COUNT OF HEADS AND TAILS
-while [ $counter1 -le 10 -a $counter2 -le 10 ]
+while [ $headCounter -le 10 -a $tailCounter -le 10 ]
 do
 	flipCheck=$(( RANDOM % 2 ))
 	case $flipCheck in
 		$head)
-			((counter1++))
+			((headCounter++))
 		;;
 		*)
-			((counter2++))
+			((tailCounter++))
 		;;
 	esac
 done
 
 # CHECK FLIP COIN WINNER
-if [ $counter1 -eq 11 ]
+if [ $headCounter -eq 11 ]
 then
 	echo Head Wins
-elif [$counter2 -eq 11 ]
+elif [ $tailCounter -eq 11 ]
 then
 	echo Tail Wins
 else
